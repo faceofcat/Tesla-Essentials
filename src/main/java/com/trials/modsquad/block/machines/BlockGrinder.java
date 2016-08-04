96f05a3ee29871d5f5a9c533452c5193ab1b6079
Gabriel Tofvesson <gabriel.tofvesson@hotmail.com>
2016-08-04 15:48:44 +0200

    Basic cable stuff

--- src/main/java/com/trials/modsquad/block/machines/BlockGrinder.java
+++ src/main/java/com/trials/modsquad/block/machines/BlockGrinder.java
@@ -1,23 +1,59 @@
 package com.trials.modsquad.block.machines;
 
+import com.trials.modsquad.ModSquad;
+import com.trials.modsquad.block.TileEntities.TileGrinder;
 import net.minecraft.block.BlockContainer;
-import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class BlockGrinder extends BlockContainer{
+import javax.annotation.Nullable;
+import java.util.concurrent.ThreadLocalRandom;
 
-    protected BlockGrinder(Material materialIn) {
-        super(materialIn);
-    }
+public class BlockGrinder extends BlockContainer {
 
-    protected BlockGrinder(Material materialIn, MapColor color) {
-        super(materialIn, color);
+
+    protected BlockGrinder() {
+        super(Material.IRON);
     }
 
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
-        return null;
+        return new TileGrinder();
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
+        if(worldIn.getTileEntity(pos) == null || playerIn.isSneaking()) return false;
+        playerIn.openGui(ModSquad.instance, 0, worldIn, pos.getX(), pos.getY(), pos.getZ());
+        return true;
+    }
+
+    @Override
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state) { // Drop items when block breaks
+        ThreadLocalRandom rand = ThreadLocalRandom.current();
+        TileEntity t = worldIn.getTileEntity(pos);
+        if(!(t instanceof IInventory)) return;
+        ItemStack s;
+        for(int i = 0; i<((IInventory) t).getSizeInventory(); ++i)
+            if((s = ((IInventory) t).getStackInSlot(i))!=null && s.stackSize > 0){
+                EntityItem item = new EntityItem(worldIn, rand.nextFloat()*0.8f+0.1f, rand.nextFloat()*.8f+.1f, rand.nextFloat()*.8f+.1f,
+                        new ItemStack(s.getItem(), s.stackSize, s.getItemDamage()));
+                if(s.hasTagCompound() && s.getTagCompound()!=null) item.getEntityItem().setTagCompound(s.getTagCompound().copy());
+                item.motionX = rand.nextGaussian() * .05f;
+                item.motionY = rand.nextGaussian() * .05f + .2f;
+                item.motionZ = rand.nextGaussian() * .05f;
+                worldIn.spawnEntityInWorld(item);
+                s.stackSize = 0;
+            }
+        super.breakBlock(worldIn, pos, state);
     }
 }
--- src/main/java/com/trials/modsquad/gui/GUIHandler.java
+++ src/main/java/com/trials/modsquad/gui/GUIHandler.java
@@ -1,6 +1,9 @@
 package com.trials.modsquad.gui;
 
+import com.trials.modsquad.Ref;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 
@@ -9,6 +12,7 @@ public class GUIHandler implements IGuiHandler {
 
     @Override
     public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        TileEntity e = world.getTileEntity(new BlockPos(x, y, z));
         return null; //TODO: Fix this
     }
 
