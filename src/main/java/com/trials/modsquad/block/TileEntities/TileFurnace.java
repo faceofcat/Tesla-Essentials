6609b5a13236a587f2c25d662b1a5d016e475cf9
tjkenmate <tjkenmate@gmail.com>
2016-08-04 08:46:23 -0400

    updated The tile  furnus

--- src/main/java/com/trials/modsquad/block/TileEntities/TileFurnace.java
+++ src/main/java/com/trials/modsquad/block/TileEntities/TileFurnace.java
@@ -2,10 +2,74 @@ package com.trials.modsquad.block.TileEntities;
 
 import net.darkhax.tesla.api.ITeslaConsumer;
 import net.darkhax.tesla.api.ITeslaHolder;
+import net.darkhax.tesla.api.implementation.BaseTeslaContainer;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.*;
+import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.tileentity.TileEntityFurnace;
 
 /**
  * Created by Tjeltigre on 8/4/2016.
  */
 public class TileFurnace extends TileEntityFurnace implements ITeslaConsumer, ITeslaHolder {
+    private BaseTeslaContainer container;
+    private static final int[] SLOTS_TOP = new int[] {0};
+    private static final int[] SLOTS_BOTTOM = new int[] {2, 1};
+    private static final int[] SLOTS_SIDES = new int[] {1};
+    /** The ItemStacks that hold the items currently being used in the furnace */
+    private ItemStack[] furnaceItemStacks = new ItemStack[3];
+    /** The number of ticks that the furnace will keep burning */
+    private int furnaceBurnTime;
+    /** The number of ticks that a fresh copy of the currently-burning item would keep the furnace burning for */
+    private int currentItemBurnTime;
+    private int cookTime;
+    private int totalCookTime;
+    private String furnaceCustomName;
+    public TileFurnace(){
+        container = new BaseTeslaContainer();
+    }
+
+    @Override
+    private boolean canSmelt()
+    {
+        if (this.furnaceItemStacks[0] == null)
+        {
+            return false;
+        }
+        else
+        {
+            ItemStack itemstack = FurnaceRecipes.instance().getSmeltingResult(this.furnaceItemStacks[0]);
+            if (itemstack == null) return false;
+            if (this.furnaceItemStacks[2] == null) return true;
+            if (!this.furnaceItemStacks[2].isItemEqual(itemstack)) return false;
+            int result = furnaceItemStacks[2].stackSize + itemstack.stackSize;
+            return result <= getInventoryStackLimit() && result <= this.furnaceItemStacks[2].getMaxStackSize(); //Forge BugFix: Make it respect stack sizes properly.
+        }
+    }
+
+    public static int getItemBurnTime()
+    {
+        if(this.getStoredPower() > 0) {
+            container.takePower(10, false);
+            return 1;
+        }
+    }
+
+    @Override
+    public long givePower(long power, boolean simulated) {
+        return container.givePower(power ,simulated);
+    }
+
+    @Override
+    public long getStoredPower() {
+        return container.getStoredPower();
+    }
+
+    @Override
+    public long getCapacity() {
+        return container.getCapacity();
+    }
 }
