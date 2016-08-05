b756cdbad85b6b03567e29730d7b900238e1752e
Gabriel Tofvesson <gabriel.tofvesson@hotmail.com>
2016-08-05 21:43:55 +0200

    Updated textures

--- src/main/java/com/trials/modsquad/block/machines/BlockElectricFurnace.java
+++ src/main/java/com/trials/modsquad/block/machines/BlockElectricFurnace.java
@@ -9,7 +9,9 @@ import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
@@ -17,25 +19,34 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import javax.annotation.Nullable;
+import java.util.List;
+
 import static com.trials.modsquad.Ref.GUI_ID_FURNACE;
 
 public class BlockElectricFurnace extends BlockGrinder {
 
-    public static final PropertyEnum ACTIVE = PropertyEnum.create("active", ActiveState.class);
+    public static final PropertyEnum ACTIVITY = PropertyEnum.create("active", ActiveState.class);
 
     public BlockElectricFurnace(String s, String s1) {
         super(s, s1);
         setCreativeTab(Ref.tabModSquad);
+        setDefaultState(blockState.getBaseState().withProperty(ACTIVITY, ActiveState.INACTIVE));
     }
 
     @Override
     protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, ACTIVE);
+        return new BlockStateContainer(this, ACTIVITY);
     }
 
     @Override
     public int getMetaFromState(IBlockState state) { // Blynd3 IS THE STUPIDEST PERSON IN THE YOONEEVERSE
-        return ((ActiveState) state.getValue(ACTIVE)).getID();
+        return ((ActiveState) state.getValue(ACTIVITY)).getID();
+    }
+
+    @Override
+    public void getSubBlocks(Item itemIn, CreativeTabs tab, List<ItemStack> list) {
+        list.add(new ItemStack(itemIn, 1, 0));
+        list.add(new ItemStack(itemIn, 1, 1));
     }
 
     @Override
