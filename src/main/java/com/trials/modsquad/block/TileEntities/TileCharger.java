568a3bf5702344b23257e8dc3fe6912429da3ea9
Gabriel Tofvesson <gabriel.tofvesson@hotmail.com>
2016-08-06 01:04:08 +0200

    Updated textures

--- src/main/java/com/trials/modsquad/block/TileEntities/TileCharger.java
+++ src/main/java/com/trials/modsquad/block/TileEntities/TileCharger.java
@@ -14,6 +14,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 
 import javax.annotation.Nullable;
 import java.lang.reflect.Field;
@@ -157,7 +158,8 @@ public class TileCharger extends TileEntity implements IInventory, ITeslaProduce
 
     @Override
     public void update() {
-        System.out.println(item != null && item.hasCapability(TeslaCapabilities.CAPABILITY_HOLDER, EnumFacing.DOWN)?item+" has the capability!":item+" does not have the capability!");
+        System.out.println(item != null && item.getItem() instanceof ICapabilityProvider &&
+                ((ICapabilityProvider) item.getItem()).hasCapability(TeslaCapabilities.CAPABILITY_HOLDER, EnumFacing.DOWN)?item+" has the capability!":item+" does not have the capability!");
     }
 
     @Override
--- src/main/java/com/trials/modsquad/block/machines/BlockGrinder.java
+++ src/main/java/com/trials/modsquad/block/machines/BlockGrinder.java
@@ -15,7 +15,6 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-
 import javax.annotation.Nullable;
 import java.util.concurrent.ThreadLocalRandom;
 
