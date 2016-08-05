bbaee1c2519487174bc98912af695288c52af026
Bread10-Minecraft <bread10minecraft@gmail.com>
2016-08-05 15:29:55 -0400

    Charger work

--- src/main/java/com/trials/modsquad/block/TileEntities/TileCharger.java
+++ src/main/java/com/trials/modsquad/block/TileEntities/TileCharger.java
@@ -25,11 +25,6 @@ import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class TileCharger extends TileEntity implements IInventory, ITeslaConsumer, ITeslaHolder, ITickable, ISidedInventory {
-    // Primitives
-    private int workTime = 0;
-    private int grindTime;
-    public static final int DEFAULT_GRIND_TIME = 60;
-    private boolean isGrinding = false;
 
     // Objects
     private final ItemStack[] inventory; // I'm an idiot
@@ -87,6 +82,12 @@ public class TileCharger extends TileEntity implements IInventory, ITeslaConsume
         return s;
     }
 
+    @Override
+    public void setField(int id, int value) {}
+
+    @Override
+    public int getField(int id) { return id; }
+
     @Nullable
     @Override
     public ItemStack removeStackFromSlot(int index) {
@@ -136,46 +137,12 @@ public class TileCharger extends TileEntity implements IInventory, ITeslaConsume
     }
 
     @Override
-    public int getField(int id) {
-        switch (id){
-            case 0:
-                return workTime;
-        }
-        return 0;
-    }
-
-    @Override
-    public void setField(int id, int value) {
-        switch (id){
-            case 0:
-                workTime = value;
-                break;
-            case 1:
-                try{
-                    Field f = BaseTeslaContainer.class.getDeclaredField("stored");
-                    f.setAccessible(true);
-                    f.setLong(container, value);
-                }catch(Exception e){}
-                break;
-            case 2:
-                container.setCapacity(value);
-                break;
-            case 3:
-                container.setInputRate(value);
-                break;
-            case 4:
-                container.setOutputRate(value);
-        }
-    }
-
-    @Override
     public int getFieldCount() {
         return 5; //TODO: Update if more stuff is added
     }
 
     @Override
     public void clear() {
-        workTime = 0;
         try{
             Field f = BaseTeslaContainer.class.getDeclaredField("stored");
             f.setAccessible(true);
@@ -222,46 +189,7 @@ public class TileCharger extends TileEntity implements IInventory, ITeslaConsume
         if(container.getStoredPower()<container.getCapacity()){
             attemptDrawEnergy(); //Try to draw energy from adjacent blocks
         }
-        if(isGrinding){
-            worldObj.spawnParticle(EnumParticleTypes.BLOCK_DUST, pos.getX(), pos.getY()+1, pos.getZ(),
-                    random.nextGaussian() * .05, random.nextGaussian() * .05+.2,random.nextGaussian() * .05);
-            worldObj.spawnParticle(EnumParticleTypes.BLOCK_DUST, pos.getX(), pos.getY()+1, pos.getZ(),
-                    random.nextGaussian() * .05, random.nextGaussian() * .05+.2,random.nextGaussian() * .05);
-            worldObj.spawnParticle(EnumParticleTypes.BLOCK_DUST, pos.getX(), pos.getY()+1, pos.getZ(),
-                    random.nextGaussian() * .05, random.nextGaussian() * .05+.2,random.nextGaussian() * .05);
-            if(grindTime == 0){
-                //TODO: Update texture
-                try{ //Troll tj
-                    Field f = Class.forName("com.trials.modsquad.Recipes.TeslaRegistry.TeslaCraftingHandler").getDeclaredField("grinderRecipeList");
-                    f.setAccessible(true);
-                    //noinspection unchecked
-                    for(GrinderRecipe g : (List<GrinderRecipe>) f.get(TeslaRegistry.teslaRegistry))
-                        if(g.getInput().getItem().equals(inventory[0].getItem()) &&
-                                (inventory[1]==null || (inventory[1].getItem().equals(g.getOutput()) &&
-                                        g.getAmount()+inventory[1].stackSize<=64))){
-                            ItemStack i = new ItemStack(g.getOutput().getItem(), inventory[1].stackSize+g.getAmount());
-                            if(g.getOutput().hasTagCompound() && g.getOutput().getTagCompound()!=null)
-                                i.setTagCompound(g.getOutput().getTagCompound());
-                            inventory[1] = i;
-                        }else return;
 
-                }catch(Exception ignored){ System.out.println("SOMETHING WENT EXTREMELY WRONG! GO MAKE SURE YOUR HOUSE ISN'T ON FIRE!!!"); }
-            }else --grindTime;
-        }else if(inventory[0]!=null){
-            try{
-                Field f = Class.forName("com.trials.modsquad.Recipes.TeslaRegistry.TeslaCraftingHandler").getDeclaredField("grinderRecipeList");
-                f.setAccessible(true);
-                List<GrinderRecipe> r = ((List<GrinderRecipe>) f.get(TeslaRegistry.teslaRegistry));
-                System.out.println("Raw tesla registry: "+r);
-                for(GrinderRecipe g : r)
-                    if(g.getInput().getItem().equals(inventory[0].getItem())){
-                        isGrinding = true;
-                        grindTime = DEFAULT_GRIND_TIME;
-                        System.out.println(pos+" started grinding!");
-                        //TODO: Update texture
-                    }
-            }catch(Exception e){} // Stupid exception that will never be thrown
-        }
 
 
     }
