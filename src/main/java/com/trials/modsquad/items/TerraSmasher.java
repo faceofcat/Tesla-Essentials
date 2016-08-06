6f659a12e6764c07f65f59613b63805fd27845d9
tjkenmate <tjkenmate@gmail.com>
2016-08-05 21:25:28 -0400

    HAHA MULTI TOOL

--- src/main/java/com/trials/modsquad/items/TerraSmasher.java
+++ src/main/java/com/trials/modsquad/items/TerraSmasher.java
@@ -5,8 +5,10 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
+import net.minecraftforge.common.util.EnumHelper;
 
 import java.lang.reflect.Field;
 import java.util.Set;
@@ -16,9 +18,9 @@ import java.util.Set;
  */
 public class TerraSmasher extends ItemTool {
     public static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(new Block[] {Blocks.ACTIVATOR_RAIL, Blocks.COAL_ORE, Blocks.COBBLESTONE, Blocks.DETECTOR_RAIL, Blocks.DIAMOND_BLOCK, Blocks.DIAMOND_ORE, Blocks.DOUBLE_STONE_SLAB, Blocks.GOLDEN_RAIL, Blocks.GOLD_BLOCK, Blocks.GOLD_ORE, Blocks.ICE, Blocks.IRON_BLOCK, Blocks.IRON_ORE, Blocks.LAPIS_BLOCK, Blocks.LAPIS_ORE, Blocks.LIT_REDSTONE_ORE, Blocks.MOSSY_COBBLESTONE, Blocks.NETHERRACK, Blocks.PACKED_ICE, Blocks.RAIL, Blocks.REDSTONE_ORE, Blocks.SANDSTONE, Blocks.RED_SANDSTONE, Blocks.STONE, Blocks.STONE_SLAB, Blocks.STONE_BUTTON, Blocks.STONE_PRESSURE_PLATE, Blocks.CLAY, Blocks.DIRT, Blocks.FARMLAND, Blocks.GRASS, Blocks.GRAVEL, Blocks.MYCELIUM, Blocks.SAND, Blocks.SNOW, Blocks.SNOW_LAYER, Blocks.SOUL_SAND, Blocks.GRASS_PATH});
-
-    public TerraSmasher(ToolMaterial materialIn) {
-            super(1.0F, -2.8F, materialIn, EFFECTIVE_ON);
+    public static final Item.ToolMaterial TERRA_SMASHER = EnumHelper.addToolMaterial("TERRA_SMASHER", 3, 1561, 10.0F, 3.0F, 25);
+    public TerraSmasher() {
+            super(1.0F, -2.8F, TERRA_SMASHER, EFFECTIVE_ON);
         try{
             Field f = ItemTool.class.getDeclaredField("toolClass");
             f.setAccessible(true);
