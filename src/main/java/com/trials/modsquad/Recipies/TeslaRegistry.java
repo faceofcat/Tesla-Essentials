caa3114439c06a6fc05d233bc6b8aec94c213af3
tjkenmate <tjkenmate@gmail.com>
2016-08-05 21:32:52 -0400

    Craftig

--- src/main/java/com/trials/modsquad/Recipies/TeslaRegistry.java
+++ src/main/java/com/trials/modsquad/Recipies/TeslaRegistry.java
@@ -1,8 +1,12 @@
 package com.trials.modsquad.Recipies;
 
+import com.trials.modsquad.block.ModBlocks;
+import com.trials.modsquad.items.ModItems;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 import java.util.ArrayList;
 
 /**
@@ -12,7 +16,9 @@ public class TeslaRegistry{
     public static TeslaCraftingHandler teslaRegistry = new TeslaCraftingHandler();
 
     public static void traditionalCraftingRegister(){
-
+        GameRegistry.addSmelting(ModBlocks.oreCopper, new ItemStack(ModItems.ingotCopper), 0F);
+        GameRegistry.addSmelting(ModBlocks.oreLead, new ItemStack(ModItems.ingotLead), 0F);
+        GameRegistry.addSmelting(ModBlocks.oreTin, new ItemStack(ModItems.ingotTin), 0F);
     }
 
     public static void registerGrinderCrafting()
