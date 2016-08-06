9bc28fdcdcffc8d327068ab72bb42617aa68290e
tjkenmate <tjkenmate@gmail.com>
2016-08-05 22:48:22 -0400

    Tera update

--- src/main/java/com/trials/modsquad/items/TerraSmasher.java
+++ src/main/java/com/trials/modsquad/items/TerraSmasher.java
@@ -1,16 +1,22 @@
 package com.trials.modsquad.items;
 
 import com.google.common.collect.Sets;
+import net.darkhax.tesla.api.implementation.BaseTeslaContainer;
+import net.darkhax.tesla.capability.TeslaCapabilities;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
+import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.util.EnumHelper;
 
 import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -19,6 +25,7 @@ import java.util.Set;
 public class TerraSmasher extends ItemTool {
     public static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(new Block[] {Blocks.ACTIVATOR_RAIL, Blocks.COAL_ORE, Blocks.COBBLESTONE, Blocks.DETECTOR_RAIL, Blocks.DIAMOND_BLOCK, Blocks.DIAMOND_ORE, Blocks.DOUBLE_STONE_SLAB, Blocks.GOLDEN_RAIL, Blocks.GOLD_BLOCK, Blocks.GOLD_ORE, Blocks.ICE, Blocks.IRON_BLOCK, Blocks.IRON_ORE, Blocks.LAPIS_BLOCK, Blocks.LAPIS_ORE, Blocks.LIT_REDSTONE_ORE, Blocks.MOSSY_COBBLESTONE, Blocks.NETHERRACK, Blocks.PACKED_ICE, Blocks.RAIL, Blocks.REDSTONE_ORE, Blocks.SANDSTONE, Blocks.RED_SANDSTONE, Blocks.STONE, Blocks.STONE_SLAB, Blocks.STONE_BUTTON, Blocks.STONE_PRESSURE_PLATE, Blocks.CLAY, Blocks.DIRT, Blocks.FARMLAND, Blocks.GRASS, Blocks.GRAVEL, Blocks.MYCELIUM, Blocks.SAND, Blocks.SNOW, Blocks.SNOW_LAYER, Blocks.SOUL_SAND, Blocks.GRASS_PATH});
     public static final Item.ToolMaterial TERRA_SMASHER = EnumHelper.addToolMaterial("TERRA_SMASHER", 3, 1561, 10.0F, 3.0F, 25);
+    public static final int drain = 10;
     public TerraSmasher(String name, String reg) {
         super(1.0F, -2.8F, TERRA_SMASHER, EFFECTIVE_ON);
         setUnlocalizedName(name);
@@ -92,5 +99,13 @@ public class TerraSmasher extends ItemTool {
     {
         Material material = state.getMaterial();
         return material != Material.IRON && material != Material.ANVIL && material != Material.ROCK ? super.getStrVsBlock(stack, state) : this.efficiencyOnProperMaterial;
+    }
+    @Override
+    public void addInformation (ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
+
+        super.addInformation(stack, playerIn, tooltip, advanced);
+        final BaseTeslaContainer container = (BaseTeslaContainer) stack.getCapability(TeslaCapabilities.CAPABILITY_HOLDER, EnumFacing.DOWN);
+            stack.setItemDamage(((int) (container.getStoredPower()/drain)));
+        tooltip.add(I18n.format("tooltip.modsquad.terrasmasher.normal", container.getStoredPower(), container.getCapacity()));
     }
 }
