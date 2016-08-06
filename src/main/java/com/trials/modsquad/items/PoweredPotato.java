04cd0ce811084186a24f4e5d0f63590481307e15
Bread10-Minecraft <bread10minecraft@gmail.com>
2016-08-06 18:40:30 -0400

    Powered Potato!

--- src/main/java/com/trials/modsquad/items/PoweredPotato.java
+++ src/main/java/com/trials/modsquad/items/PoweredPotato.java
@@ -21,6 +21,7 @@ import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import javax.annotation.Nullable;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 import static net.darkhax.tesla.capability.TeslaCapabilities.CAPABILITY_HOLDER;
 import static net.darkhax.tesla.capability.TeslaCapabilities.CAPABILITY_PRODUCER;
@@ -28,18 +29,28 @@ import static net.darkhax.tesla.capability.TeslaCapabilities.CAPABILITY_PRODUCER
 public class PoweredPotato extends ItemFood {
 
     private final Field itemDamage;
+    private BaseTeslaContainer container = new BaseTeslaContainer();
 
     public PoweredPotato(String unlocalizedName, String registryName) {
         super(4, 0.8F, false);
         setUnlocalizedName(unlocalizedName);
         setRegistryName(registryName);
         setCreativeTab(Ref.tabModSquad);
+        setMaxStackSize(1);
+        setMaxDamage(40);
         Field f = null;
         try {
             f = ItemStack.class.getDeclaredField("itemDamage");
             f.setAccessible(true);
         } catch (NoSuchFieldException e) { }
         this.itemDamage = f;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
+        BaseTeslaContainer container = (BaseTeslaContainer) stack.getCapability(TeslaCapabilities.CAPABILITY_HOLDER, EnumFacing.DOWN);
+        tooltip.add("Power: " + container.getStoredPower() + "/" + container.getCapacity());
+        super.addInformation(stack, playerIn, tooltip, advanced);
     }
 
     @Nullable
@@ -50,6 +61,8 @@ public class PoweredPotato extends ItemFood {
             EntityPlayer entityplayer = (EntityPlayer)entityLiving;
             worldIn.playSound(null, entityplayer.posX, entityplayer.posY, entityplayer.posZ, SoundEvents.ENTITY_PLAYER_BURP, SoundCategory.PLAYERS, 0.5F, worldIn.rand.nextFloat() * 0.1F + 0.9F);
             container.takePower(50, false);
+            entityplayer.getFoodStats().setFoodLevel(entityplayer.getFoodStats().getFoodLevel() + 4);
+            entityplayer.getFoodStats().setFoodSaturationLevel(entityplayer.getFoodStats().getSaturationLevel() + 0.8F);
             this.onFoodEaten(stack, worldIn, entityplayer);
         }
         return stack;
@@ -57,7 +70,7 @@ public class PoweredPotato extends ItemFood {
 
     @Override
     public ICapabilityProvider initCapabilities(ItemStack stack, NBTTagCompound nbt) {
-        return new BaseTeslaContainerProvider(new BaseTeslaContainer(){
+        return new BaseTeslaContainerProvider(new BaseTeslaContainer(2000, 20, 20){
             @Override
             public long givePower(long Tesla, boolean simulated) {
                 setDamage(stack, 0);
