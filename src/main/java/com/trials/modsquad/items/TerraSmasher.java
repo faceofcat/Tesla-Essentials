4eecc8a4ccc1535653e9dce92203464f9878bf5c
tjkenmate <tjkenmate@gmail.com>
2016-08-05 21:07:53 -0400

    HAHA MULTI TOOL

---
+++ src/main/java/com/trials/modsquad/items/TerraSmasher.java
@@ -0,0 +1,91 @@
+package com.trials.modsquad.items;
+
+import com.google.common.collect.Sets;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
+
+import java.lang.reflect.Field;
+import java.util.Set;
+
+/**
+ * Created by Tjeltigre on 8/5/2016.
+ */
+public class TerraSmasher extends ItemTool {
+    public static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(new Block[] {Blocks.ACTIVATOR_RAIL, Blocks.COAL_ORE, Blocks.COBBLESTONE, Blocks.DETECTOR_RAIL, Blocks.DIAMOND_BLOCK, Blocks.DIAMOND_ORE, Blocks.DOUBLE_STONE_SLAB, Blocks.GOLDEN_RAIL, Blocks.GOLD_BLOCK, Blocks.GOLD_ORE, Blocks.ICE, Blocks.IRON_BLOCK, Blocks.IRON_ORE, Blocks.LAPIS_BLOCK, Blocks.LAPIS_ORE, Blocks.LIT_REDSTONE_ORE, Blocks.MOSSY_COBBLESTONE, Blocks.NETHERRACK, Blocks.PACKED_ICE, Blocks.RAIL, Blocks.REDSTONE_ORE, Blocks.SANDSTONE, Blocks.RED_SANDSTONE, Blocks.STONE, Blocks.STONE_SLAB, Blocks.STONE_BUTTON, Blocks.STONE_PRESSURE_PLATE, Blocks.CLAY, Blocks.DIRT, Blocks.FARMLAND, Blocks.GRASS, Blocks.GRAVEL, Blocks.MYCELIUM, Blocks.SAND, Blocks.SNOW, Blocks.SNOW_LAYER, Blocks.SOUL_SAND, Blocks.GRASS_PATH});
+
+    public TerraSmasher(ToolMaterial materialIn) {
+            super(1.0F, -2.8F, materialIn, EFFECTIVE_ON);
+        try{
+            Field f = ItemTool.class.getDeclaredField("toolClass");
+            f.setAccessible(true);
+            f.set(this, new String("pickaxe"));
+        }catch(Exception e){}
+        }
+
+        /**
+         * Check whether this Item can harvest the given Block
+         */
+    public boolean canHarvestBlock(IBlockState blockIn)
+    {
+        Block block = blockIn.getBlock();
+
+        if (block == Blocks.OBSIDIAN)
+        {
+            return this.toolMaterial.getHarvestLevel() == 8;
+        }
+        else if (block != Blocks.DIAMOND_BLOCK && block != Blocks.DIAMOND_ORE)
+        {
+            if (block != Blocks.EMERALD_ORE && block != Blocks.EMERALD_BLOCK)
+            {
+                if (block != Blocks.GOLD_BLOCK && block != Blocks.GOLD_ORE)
+                {
+                    if (block != Blocks.IRON_BLOCK && block != Blocks.IRON_ORE)
+                    {
+                        if (block != Blocks.LAPIS_BLOCK && block != Blocks.LAPIS_ORE)
+                        {
+                            if (block != Blocks.REDSTONE_ORE && block != Blocks.LIT_REDSTONE_ORE)
+                            {
+                                Material material = blockIn.getMaterial();
+                                return material == Material.ROCK ? true : (material == Material.IRON ? true : material == Material.ANVIL);
+                            }
+                            else
+                            {
+                                return this.toolMaterial.getHarvestLevel() >= 2;
+                            }
+                        }
+                        else
+                        {
+                            return this.toolMaterial.getHarvestLevel() >= 1;
+                        }
+                    }
+                    else
+                    {
+                        return this.toolMaterial.getHarvestLevel() >= 1;
+                    }
+                }
+                else
+                {
+                    return this.toolMaterial.getHarvestLevel() >= 2;
+                }
+            }
+            else
+            {
+                return this.toolMaterial.getHarvestLevel() >= 2;
+            }
+        }
+        else
+        {
+            return this.toolMaterial.getHarvestLevel() >= 2;
+        }
+    }
+
+    public float getStrVsBlock(ItemStack stack, IBlockState state)
+    {
+        Material material = state.getMaterial();
+        return material != Material.IRON && material != Material.ANVIL && material != Material.ROCK ? super.getStrVsBlock(stack, state) : this.efficiencyOnProperMaterial;
+    }
+}
